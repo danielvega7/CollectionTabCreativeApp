6f85b74928209c76c4706fdca80b847bb4275a2b
Daniel Vega <daniel.vega23@d155.org>
2021-11-04 08:10:28 -0500

    started on creative app

---
+++ CollectionTabCreativeApp/File.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  CollectionTabCreativeApp
+//
+//  Created by DANIEL VEGA on 11/4/21.
+//
+
+import Foundation
+
+public class User{
+    var userName: String
+    var password: String
+    
+    init(u: String, p: String) {
+        userName = u
+        password = p
+    }
+    
+    
+}
--- CollectionTabCreativeApp/ViewController.swift
+++ CollectionTabCreativeApp/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public class StatVar{
+   
+    static var userArray = [User]()
+    
+}
 
+class ViewController: UIViewController, UITextViewDelegate {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
     }
 
+    @IBAction func createUserAction(_ sender: Any) {
+        if checkTextField(tf: usernameTextField) && checkTextField(tf: passwordTextField) {
+            StatVar.userArray.append(User(u: usernameTextField.text!, p: passwordTextField.text!))
+        }
+        else{
+            let alert = UIAlertController(title: "Error", message: "Username of Password invalid", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Revisit", style: .cancel, handler: nil)
+            alert.addAction(action)
+            self.present(alert, animated: true)
+        }
+    }
+    
+    
+    func checkTextField(tf: UITextField) -> Bool{
+        if tf.text == "" {
+           return false
+        }
+        else {
+            return true
+        }
+    }
 
 }
 
